da673502b5057e46cdde8cde2494a3df622e3ff6
zhou cheng <zhoucheng@kingsoft.com>
2018-05-28 10:50:33 +0800

    remove othen model

--- client.go
+++ client.go
@@ -20,5 +20,4 @@ var rpc *string
 func init() {
 	masterHost = flag.String("master-host", "127.0.0.1", "Host or IP address of locust master for distributed load testing. Defaults to 127.0.0.1.")
 	masterPort = flag.Int("master-port", 5557, "The port to connect to that is used by the locust master for distributed load testing. Defaults to 5557.")
-	rpc = flag.String("rpc", "zeromq", "Choose zeromq or tcp socket to communicate with master, don't mix them up.")
 }
